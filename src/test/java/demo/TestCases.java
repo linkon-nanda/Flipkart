650b2152ff80b85d7f621a25cff324910112729f
Linkon Nanda <linkon.nanda11@gmail.com>
2025-04-10 21:50:13 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -6,33 +6,98 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
 import java.util.logging.Level;
-
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    WebDriverWait wait;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
+    @Test
+    public void testCase01() throws InterruptedException {
+
+        System.out.println("Start TestCase01");
+
+       //to open and peint the url
+        Wrappers.openUrl(driver, " https://www.flipkart.com/");
+
+        //entertext method is to send text to an element
+        Wrappers.enterText(driver, By.xpath("//input[@name = 'q']"), "Washing Machine");
+
+        //this method is to wait for an element until it's visible
+        Wrappers.waitForAnElement(driver, By.xpath("//div[normalize-space()='Popularity']"), 10);
+
+        //this method is for locating and clicking an element
+        Wrappers.clickElement(driver, By.xpath("//div[normalize-space()='Popularity']"));
+
+        //this method is to count the no of products having rating less than or equal to 4 stars
+        Wrappers.checkCountFourStarOrLessRating(driver, By.xpath("//div[@class='col col-7-12']//div[@class='XQDdHH']"));
+
+        System.out.println("End TestCase01");
+
+    }
+
+    @Test
+    public void testCase02() throws InterruptedException {
+
+        System.out.println("Start TestCase02");
+
+        Wrappers.openUrl(driver, " https://www.flipkart.com/");
+
+        Wrappers.enterText(driver, By.xpath("//input[@name = 'q']"), "iPhone");
+
+        Wrappers.waitForAnElement(driver, By.xpath("//div[normalize-space()='Popularity']"), 10);
+
+        //this method is to fetch the title of the products having more than 17% discount
+        Wrappers.fetchTitleWithDiscMoreThan17Perc(driver,
+                By.xpath("//div[@class='col col-5-12 BfVC2z']//div[@class='UkUFwK']"),
+                By.xpath("//div[@class='col col-7-12']//div[@class='KzDlHZ']"));
+
+        System.out.println("End TestCase02");
+    }
+
+    @Test
+    public void testCase03() throws InterruptedException {
+
+        System.out.println("Start TestCase03");
+
+        Wrappers.openUrl(driver, " https://www.flipkart.com/");
+
+        Wrappers.enterText(driver, By.xpath("//input[@name = 'q']"), "Coffee Mug");
+
+        Wrappers.waitForAnElement(driver, By.xpath("(//div[@class='XqNaEv'])[1]"), 10);
+
+        //this method is to click on an element by using Actions class
+        Wrappers.clickUsingActionsClass(driver, By.xpath("(//div[@class='XqNaEv'])[1]"));
+        Thread.sleep(3000);
+
+        //this method is to print title,imageUrl and reviews for the top 5 products having highest reviews
+        Wrappers.fetchDetailsOfHighestRatedProduct(driver, By.xpath("//div[@class='slAVV4']"),
+                By.xpath(".//a[@class='wjcEIp']"), By.xpath(".//a[@class='wjcEIp']"),
+                By.xpath(".//span[@class='Wphh3N']"));
+
+        System.out.println("End TestCase03");
+    }
+
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -46,18 +111,17 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         driver.manage().window().maximize();
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
-
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,22 +1,242 @@
 package demo.wrappers;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Wrappers {
+
     /*
      * Write your selenium wrappers here
      */
+
+    // wrapper method for opening and printing the url
+    public static void openUrl(ChromeDriver driver, String url) {
+
+        try {
+
+            driver.get(url);
+            Thread.sleep(3000);
+            String currentUrl = driver.getCurrentUrl();
+            System.out.println("Current Page URL: " + currentUrl);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    // wrapper method for explicitly waiting for an element
+    public static void waitForAnElement(ChromeDriver driver, By locator, int timeOutInSec) {
+
+        try {
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOutInSec));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    // wrapper method to enter a text
+    public static void enterText(ChromeDriver driver, By locator, String textToEnter) {
+
+        try {
+
+            WebElement element = driver.findElement(locator);
+            Thread.sleep(500);
+            element.clear();
+            Thread.sleep(500);
+            element.sendKeys(textToEnter);
+            Thread.sleep(500);
+            element.sendKeys(Keys.ENTER);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+    // wrapper method to click element when it is displayed and enabled
+    public static void clickElement(ChromeDriver driver, By locator) {
+
+        try {
+
+            WebElement element = driver.findElement(locator);
+            if (element.isDisplayed() && element.isEnabled()) {
+
+                element.click();
+                Thread.sleep(1000);
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    // wrapper method for click using Actions class
+    public static void clickUsingActionsClass(ChromeDriver driver, By locator) {
+
+        try {
+
+            WebElement element = driver.findElement(locator);
+            Actions actions = new Actions(driver);
+            actions.moveToElement(element).click().perform();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    // wrapper method to print the count of products having less than 0r equal to 4
+    // star rating
+    public static void checkCountFourStarOrLessRating(ChromeDriver driver, By locator) {
+
+        try {
+
+            int ratingCount = 0;
+            List<String> allRatings = new ArrayList<>();
+            List<WebElement> ratings = driver.findElements(locator);
+            for (WebElement rating : ratings) {
+
+                String ratingText = rating.getText();
+                double ratingValue = Double.parseDouble(ratingText);
+
+                allRatings.add(ratingText);
+
+                if (ratingValue <= 4) {
+
+                    ratingCount++;
+                }
+            }
+
+            System.out.println("All Ratings available are:" + allRatings);
+            System.out.println("The count of items with rating less than or equal to 4 stars:" + ratingCount);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+    // wrapper method to print the title when discount is more than 17%
+    public static void fetchTitleWithDiscMoreThan17Perc(ChromeDriver driver, By discPercLocator, By titlesLocator2) {
+
+        try {
+
+            boolean value = false;
+            List<WebElement> discPercElement = driver
+                    .findElements(discPercLocator);
+            List<WebElement> titlesElement = driver.findElements(titlesLocator2);
+
+            for (int i = 0; i < discPercElement.size(); i++) {
+
+                String discText = discPercElement.get(i).getText();
+                String titleText = titlesElement.get(i).getText();
+                String onlyDisc = discText.replace("% off", "");
+                int discValue = Integer.parseInt(onlyDisc);
+
+                if (discValue > 17) {
+
+                    value = true;
+                    System.out.println("The discount % is:" + discText);
+                    System.out.println("The titles with more than 17% discount is:" + titleText);
+                }
+            }
+
+            if (!value) {
+
+                System.out.println("No titles available having discount more than 17%");
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    // wrapper class to print title,imageurls and reviews of top 5 rated products
+    public static void fetchDetailsOfHighestRatedProduct(ChromeDriver driver, By productsLocator, By titlesLocator,
+            By imageUrlsLocator, By reviewsLocator) {
+
+        try {
+
+            List<String> titles = new ArrayList<>();
+            List<String> imageUrls = new ArrayList<>();
+            List<Integer> reviews = new ArrayList<>();
+
+            List<WebElement> products = driver.findElements(productsLocator);
+
+            for (WebElement product : products) {
+
+                try {
+                    WebElement productTitle = product.findElement(titlesLocator);
+                    String title = productTitle.getAttribute("title");
+
+                    WebElement productImageUrl = product.findElement(imageUrlsLocator);
+                    String imageUrl = productImageUrl.getAttribute("href");
+
+                    WebElement productReview = product.findElement(reviewsLocator);
+                    String reviewText = productReview.getText().replaceAll("[^\\d]", "");
+                    int review = Integer.parseInt(reviewText);
+
+                    titles.add(title);
+                    imageUrls.add(imageUrl);
+                    reviews.add(review);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+                }
+
+            }
+
+            for (int i = 0; i < reviews.size(); i++) {
+
+                for (int j = i + 1; j < reviews.size(); j++) {
+
+                    if (reviews.get(j) > reviews.get(i)) {
+
+                        int tempReview = reviews.get(i);
+                        reviews.set(i, reviews.get(j));
+                        reviews.set(j, tempReview);
+
+                        String tempTitle = titles.get(i);
+                        titles.set(i, titles.get(j));
+                        titles.set(j, tempTitle);
+
+                        String tempUrl = imageUrls.get(i);
+                        imageUrls.set(i, imageUrls.get(j));
+                        imageUrls.set(j, tempUrl);
+
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < 5; i++) {
+
+                System.out.println("Top 5 titles based on reivew: " + titles.get(i));
+                System.out.println("Top 5 imageUrls based on reivew: " + imageUrls.get(i));
+                System.out.println("Top 5 reviews: " + reviews.get(i));
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
 }
